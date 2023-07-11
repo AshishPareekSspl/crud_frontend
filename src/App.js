3a93095ab3dca23402b566ddfb02e7f39e3fe074
Ashish Kumar Pareek <>
2023-07-11 15:34:50 +0530

    inittial commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Header from './component/Header';
 import CreatePost from './component/CreatePost';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ToastContainer position='top-center' />
       <Routes>
         <Route path='/' element={<CreatePost />}/>
       </Routes>
+
     </div>
   );
 }
--- src/component/CreatePost.jsx
+++ src/component/CreatePost.jsx
@@ -1,37 +1,70 @@
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
+import axios from "axios"
+import { toast } from "react-toastify"
 
-const CreatePost = () => {
- const [postData, setPostData] = useState({
+const CreatePost = () => { 
+  const formRef = useRef(null)
+  const [postData, setPostData] = useState({
     title: "",
     date: "",
-    image:''
- })
+    image: "",
+  })
 
- const postHandler = (e) =>{
-    const {name,value} = e.target
-    setPostData(prev=>({...prev,[name]:value}))
- }
+  const postHandler = (e) => {
+    const { name, value } = e.target
+    setPostData((prev) => ({ ...prev, [name]: value }))
+  }
 
- const imageHandler = (e)=>{
-    setPostData(prev=>({...prev, image:e.target.files[0]}))
- }
+  const imageHandler = (e) => {
+    setPostData((prev) => ({ ...prev, image: e.target.files[0] }))
+  }
 
- const submitHamdler = () =>{
-  
- }
+  const submitHamdler = (e) => {
+    e.preventDefault()
+    const formData = new FormData()
+
+    formData.append("title",postData.title)
+    formData.append("date",postData.date)
+    formData.append("image",postData.image)
+
+    if (postData.title&&postData.date&&postData.image) {
+      axios.post("http://localhost:6500/api/create-post",formData).then((res)=>{
+        console.log(res.data);
+        if (res.data.success) {
+          formRef.current.reset()
+          toast.success("Post Added successfully")
+          setPostData({title:"",date:"",image:""})
+        }
+      })
+    }else{
+      toast.error("All flieds are required")
+    }
+
+  }
 
   return (
     <div className="createPostContainer">
       <div className="createPost">
-        <h1>
-            Create a new post
-        </h1>
-        <div className="postData">
-            <input type="text" name="title" onChange={postHandler} value={postData.title} />
-            <input type="date" name="date" onChange={postHandler} value={postData.date} />
-            <input type="file" name="image" onChange={imageHandler}/>
-            <input type="button" value="Submit" onClick={submitHamdler} />
-        </div>
+        <h1>Create A New Post</h1>
+        <form className="postDataForm" ref={formRef}>
+          <input
+          placeholder="Title"
+            type="text"
+            name="title"
+            onChange={postHandler}
+            value={postData.title}
+            required
+          />
+          <input
+            type="date"
+            name="date"
+            onChange={postHandler}
+            value={postData.date}
+            required
+          />
+          <input type="file" name="image" onChange={imageHandler} required />
+          <button onClick={submitHamdler} >Submit</button>
+        </form>
       </div>
     </div>
   )
